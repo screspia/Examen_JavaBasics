9eb064b016cfa3d74161cb7ed825761e21ed4420
screspia <s.crespi.a@gmail.com>
2018-09-28 00:38:58 +0200

    Update GetLastThursday.java

--- src/main/java/com/privalia/util/GetLastThursday.java
+++ src/main/java/com/privalia/util/GetLastThursday.java
@@ -7,8 +7,8 @@ public class GetLastThursday {
 
 	public static int getLastThursday( int month, int year ) {
 		   Calendar cal = Calendar.getInstance();
-		   cal.set( year, month + 1, 1 );
-		   cal.add( Calendar.DAY_OF_MONTH, -( cal.get( Calendar.DAY_OF_WEEK ) % 7 + 2 ) );
+		   cal.set(year, month , 1);
+		   cal.add(Calendar.DAY_OF_MONTH, - (cal.get(Calendar.DAY_OF_WEEK) % 7 + 2));
 		   System.out.println(cal.getTime());
 		   return cal.get(Calendar.DAY_OF_MONTH);
 		}
